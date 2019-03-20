42583923dbcaf000ad4671ce5d3b949ee7d2ed8e
Virgile Patrignani <virgile_patrignani@hotmail.com>
2019-03-20 19:06:41 +0000

    added character count in scripts.Error message still missing

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,12 @@
-console.log("test")
+$(document).ready(function() {
+    $("main form textarea").on("input", function(event) {
+       const maxCharLenght = 140;
+       const currentCharLength = $(this).val().length;
+        if (currentCharLength > maxCharLenght) {
+            console.log("Max characters execeeded")
+        } else {
+            $("main form span").text(maxCharLenght - currentCharLength)
+        }
+    });
+  });
+

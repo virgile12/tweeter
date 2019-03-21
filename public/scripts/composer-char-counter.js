05ba4952e174af5eae1d50136b711c1669d64a39
Virgile Patrignani <virgile_patrignani@hotmail.com>
2019-03-21 16:34:20 +0000

    added main-tweet css style

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     $("main form textarea").on("input", function(event) {
-       const maxCharLenght = 140;
+       const maxCharLength = 140;
        const currentCharLength = $(this).val().length;
-       $("main form span").text(maxCharLenght - currentCharLength)
-        if (currentCharLength > maxCharLenght) {
+       $("main form span").text(maxCharLength - currentCharLength)
+        if (currentCharLength > maxCharLength) {
             $("main form span").addClass("max-char-error")
         } else {
             $("main form span").removeClass("max-char-error")

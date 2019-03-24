c45a5f959a55ce9c19c7f73e895efa876b724222
Virgile Patrignani <virgile_patrignani@hotmail.com>
2019-03-24 18:27:40 +0000

    code readability fix in composer[...].js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    $('main form input').attr('disabled', 'disabled')
-    $('div.compose-tweet').hide()
+    $('main form input').attr('disabled', 'disabled');
+    $('div.compose-tweet').hide();
     $('nav button.btn-navbar').on('click', function() {
          $('div.compose-tweet').slideToggle( 'slow' );
          $('textarea.textspot').select();
@@ -11,22 +11,22 @@ $(document).ready(function() {
        
        $('main form span').text(maxCharLength - currentCharLength)
         if (currentCharLength > maxCharLength) {
-            $('main form span').addClass('max-char-error')
-            $('main form input').attr('disabled', 'disabled')
-            $('div.error-box').addClass('error-message').text('Warning! Tweet length exceeded.')
+            $('main form span').addClass('max-char-error');
+            $('main form input').attr('disabled', 'disabled');
+            $('div.error-box').addClass('error-message').text('Warning! Tweet length exceeded.');
             $('div.error-box').css({'color': 'red', 'float': 'right', 'padding-right': '5px', 'padding-bottom': '5px'});
-            
+    
         }  else if ( $('main form textarea').val().trim() === '') {
-            $('main form input').attr('disabled', 'disabled')
-            $('main form span').removeClass('max-char-error')
-            $('.error-message').removeClass('error-message').text('')
+            $('main form input').attr('disabled', 'disabled');
+            $('main form span').removeClass('max-char-error');
+            $('.error-message').removeClass('error-message').text('');
 
         } else {
-            $('main form span').removeClass('max-char-error')
-            $('main form input').removeAttr('disabled')
-            $('.error-message').removeClass('error-message').text('')
+            $('main form span').removeClass('max-char-error');
+            $('main form input').removeAttr('disabled');
+            $('.error-message').removeClass('error-message').text('');
         }
-    })
+    });
 
      
     });
